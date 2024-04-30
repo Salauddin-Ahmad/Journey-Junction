1f1eb94819556956c9f2a6b9a4583d6d52ee0a9c
Md. Salauddin <salauddinahmed555@gmail.com>
2024-04-30 14:45:38 +0600

    made the my list Table of details

--- src/components/MySpotsCard.jsx
+++ src/components/MySpotsCard.jsx
@@ -53,28 +53,35 @@ const handleDelete = (id) => {
     });
 };
 
-
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {item?.map((data) => (
-        <div key={data._id} className=" p-4 rounded-lg shadow-md border">
-          <img
-            className="w-[350px] h-44 rounded-md mx-auto"
-            src={data.photo}
-            alt=""
-          />
-          <h1 className="text-xl font-semibold">{data.country_Name}</h1>
-          <p className=" mt-2">{data.description}</p>
-          <div className="flex items-center justify-center gap-8 mt-2">
-          <Link to={`/updateDetails/${data._id}`}>
-          <button className="btn bg-blue-600">Update</button>
-          </Link>
-            <button onClick={() => handleDelete(data._id)} className="btn  bg-red-800">Delete</button>
-          </div>
-        </div>
-      ))}
-    </div>
+return (
+    <table className="table-auto">
+      <thead className="border">
+        <tr>
+          <th className="px-4 py-2 border-4 border-yellow-600">Photo</th>
+          <th className="px-4 py-2 border-4 border-yellow-600">Country Name</th>
+          <th className="px-4 py-2 border-4 border-yellow-600">Description</th>
+          <th className="px-4 py-2 border-4 border-yellow-600">Actions</th>
+        </tr>
+      </thead>
+      <tbody >
+        {item?.map((data) => (
+          <tr className="border" key={data._id}>
+            <td className="px-4 py-2 border-4 border-emerald-700"><img className="w-[100px] h-[100px] rounded-md" src={data.photo} alt="" /></td>
+            <td className="px-4 py-2 border-4 border-emerald-700">{data.country_Name}</td>
+            <td className="px-4 py-2 border-4 border-emerald-700">{data.description}</td>
+            <td className="px-4 py-2 border-4 border-emerald-700">
+              <Link to={`/updateDetails/${data._id}`}>
+                <button className="btn bg-blue-600">Update</button>
+              </Link>
+              <button onClick={() => handleDelete(data._id)} className="btn bg-red-800">Delete</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
+  
+
 };
 
 export default MySpotsCard;
